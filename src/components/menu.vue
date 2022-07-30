85340d35b6e50b8488586fa9763072efb29a72f2
Diegton Rodrigues <diegton@gmail.com>
2022-07-30 09:29:42 -0300

    init

---
+++ src/components/menu.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+onMounted(() => {});
+</script>
+
+<template><a class="waves-effect waves-light btn">Menu 1</a></template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import TheWelcome from "@/components/TheWelcome.vue";
+import Mennu from "@/components/menu.vue";
+
+onMounted(() => {
+  console.log(`The initial count is.`);
+});
+</script>
+
+<template>
+  <main>
+    <div class="row">
+      <div class="col s2"><Mennu /></div>
+      <div class="col s6"><TheWelcome /></div>
+    </div>
+  </main>
+</template>
