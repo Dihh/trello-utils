df36a0a89cb3f928f8dc9b212d2f9af01bc43234
Diegton Rodrigues <diegton@gmail.com>
2022-07-30 15:10:41 -0300

    final init

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/trello-utils/',
       name: 'home',
       component: HomeView
     }
