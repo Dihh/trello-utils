aa6e04aecdbb8dfeaa1209d8c98c8181abfb2cfa
Diegton Rodrigues <diegton@gmail.com>
2022-09-11 10:31:22 -0300

    dashboard nav component

---
+++ src/components/dashboard/dashboard-nav.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits(["changeView"]);
+
+function chageView(view: string) {
+  emit("changeView", view);
+}
+</script>
+
+<template>
+  <nav class="nav-extended">
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent">
+        <li class="tab">
+          <a href="#test1" @click="chageView('dashboard')">Dashboard</a>
+        </li>
+        <li class="tab">
+          <a href="#test2" @click="chageView('config')">Configuração</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
